5446a8ee7b4eb24444517fecef024f492fa72cb1
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-06-04 23:03:30 -0600

    revise linkedIn link in contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
           <div>
             <p className='text-3xl font-bold inline border-b-4 border-r-4 rounded border-[#3F72AF] text-[#DBE2EF] px-2 py-1'>Contact</p>
             <div className='flex flex-row text-[#F9F7F7] pt-4 my-4'>
-              <p>Submit the form below or shoot me a message in <a className='font-bold cursor-pointer ' href='https://www.linkedin.com/in/shuk-han-chau-835057132' target='_blank'> Linkedin </a></p>
+              <p>Submit the form below or shoot me a message in <a className='font-bold cursor-pointer ' href='https://www.linkedin.com/in/alix-shuk-han-chau-835057132/' target='_blank'> Linkedin </a></p>
             </div>
           </div>
           <input className='my-4 p-2 bg-[#ccd6f6] text-[#112D4E] rounded' type='text' placeholder='Name' name='name' required />
